51c94b9243e98c97c990d5228751507a566b0d7e
filestoreinnet <48118543+filestoreinnet@users.noreply.github.com>
2023-12-20 16:02:21 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -13,17 +13,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const fbclid = ctx.query.fbclid;
   console.log(referringURL);
 
-  // redirect if facebook is the referer or request contains fbclid
-  if (referringURL?.includes("facebook.com") || fbclid) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: `${
-          endpoint.replace(/(\/graphql\/)/, "/") + encodeURI(path as string)
-        }`,
-      },
-    };
-  }
+ 
   const query = gql`
 		{
 			post(id: "/${path}/", idType: URI) {
